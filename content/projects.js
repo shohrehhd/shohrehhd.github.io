dffe926ed03c23dd787c7470e4445581c3a1bf5f
Shohreh Haddadan <72942590+shohrehhd@users.noreply.github.com>
2026-06-09 10:37:53 -0400

    Add Disputool project a

--- content/projects.js
+++ content/projects.js
@@ -7,15 +7,15 @@ window.SITE_CONTENT.projects = [
     name: "LaiLab@Chemotimeline2024",
     desc: "Chemotherapy timeline extraction",
     link: "https://github.com/LangIntLab/Chemotimeline#"
-  }/*,
-  {
-    name: "Another Project",
-    desc: "Built with X to solve Y. Mention the result or the tech.",
-    link: ""
   },
+  {
+    name: "Disputool",
+    desc: "Demo of argumentative component annotation and detection.",
+    link: "https://disputool.uni.lu/"
+  }/*,
   {
     name: "Third Thing",
     desc: "Could be open source, a paper, a talk, or a side project.",
     link: ""
-  }*/
+  }*/*/
 ];
